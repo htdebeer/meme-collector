76980ec2cad9fd323d5099369c7ce75347f48d0b
Huub de Beer <Huub@heerdebeer.org>
2017-03-11 13:07:19 +0100

    Removed open link in period list and automatically selecting first meme in period

--- app/public/meme_collector.js
+++ app/public/meme_collector.js
@@ -20,15 +20,29 @@ const dataTableConfiguration = {
   "autoResize": false
 };
 
+const selectRowAction = function (row) {
+  const memeURL = row.find("input.link").val();
+  $("#meme_view").attr("src", memeURL);
+};
+
 $(document).ready(function () {
+  $(".table").on("init.dt", function () {
+    const table = $(this).dataTable().api();
+    if (0 < table.rows().length) {
+      const row = table.rows(0);
+      row.select();
+      selectRowAction(row.nodes().to$());
+    }
+  });
+  
   const table = $('.table').DataTable(dataTableConfiguration);
 
   $('#valid_only').change(() => table.draw());
 
   table.on("select", function (e, dt, type, indexes) {
     if (type === "row") {
-      const memeURL = table.rows(indexes).nodes().to$().find("a.action").attr("href");
-      $("#meme_view").attr("src", memeURL);
+      selectRowAction(table.rows(indexes).nodes().to$());
     }
   });
+
 });
